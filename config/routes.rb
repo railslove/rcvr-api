3a3d6be71427859295b51ed5d185b9e638f0aa95
Bonflintstone <freddy@railslove.com>
2020-05-08 20:56:07 +0200

    Cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   resources :tickets
   resources :companies
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
