37e607cbbf6da690f8635a456f09ecb000e1ee70
Bonflintstone <freddy@railslove.com>
2020-05-08 21:46:40 +0200

    Can create and update tickets

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,17 @@
 class TicketsController < ApplicationController
+  def create
+    @ticket = Ticket.create!(ticket_params)
+  end
+
+  def update
+    @ticket = Ticket.find(params[:id])
+
+    @ticket.update!(ticket_params)
+  end
+
+  private
+
+  def ticket_params
+    params.require(:ticket).permit(:id, :company_id, :entered_at, :left_at)
+  end
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,2 +1,3 @@
 class Ticket < ApplicationRecord
+  validates :id, absence: true, on: :update, if: :id_changed? # Can never update id
 end
