a6c1daa7176423c76ef53f185334cf91ba31fd56
Bonflintstone <freddy@railslove.com>
2020-05-09 23:01:24 +0200

    Fix ruby version, only set raven things on prod

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::API
-  before_action :set_raven_context
   before_action :set_default_request_format
 
   rescue_from StandardError, with: :render_internal_error
@@ -12,11 +11,6 @@ class ApplicationController < ActionController::API
     request.format = :json
   end
 
-  def set_raven_context
-    Raven.user_context(id: current_user&.id)
-    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
-  end
-
   def render_unprocessable_entity_response(exception)
     render json: exception.record.errors, status: :unprocessable_entity
   end
@@ -27,7 +21,14 @@ class ApplicationController < ActionController::API
 
   def render_internal_error(error)
     response = { message: error.message, name: error.class.name }
-    response[:backtrace] = error.backtrace
+
+    if Rails.env.production?
+      Raven.user_context(id: current_user&.id)
+      Raven.extra_context(params: params.to_unsafe_h, url: request.url)
+      Raven.capture_exception(error)
+    else
+      response[:backtrace] = error.backtrace
+    end
 
     render json: response, status: :internal_server_error
   end
