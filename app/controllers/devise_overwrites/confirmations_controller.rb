265e94f82be7ca25bbda287358e3420ecb0d053c
Bonflintstone <freddy@railslove.com>
2020-05-13 14:21:19 +0200

    Remove double render

--- app/controllers/devise_overwrites/confirmations_controller.rb
+++ app/controllers/devise_overwrites/confirmations_controller.rb
@@ -4,9 +4,9 @@ module DeviseOverwrites
 
     def after_confirmation_path_for(resource_name, resource)
       if resource.public_key.blank?
-        redirect_to 'https://rcvr.app/business/setup/key-intro'
+        'https://rcvr.app/business/setup/key-intro'
       else
-        redirect_to 'https://rcvr.app/business/dashboard'
+        'https://rcvr.app/business/dashboard'
       end
     end
   end
