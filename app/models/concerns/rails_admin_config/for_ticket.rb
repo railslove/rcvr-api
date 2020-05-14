51e553a2455ac16e5a2e9c0f316adf795f611bce
Salim Hbeiliny <salim@railslove.com>
2020-05-14 18:02:45 +0200

    export csv with column order expected for decryption

--- app/models/concerns/rails_admin_config/for_ticket.rb
+++ app/models/concerns/rails_admin_config/for_ticket.rb
@@ -4,7 +4,27 @@ module RailsAdminConfig
 
     included do
       rails_admin do
-        fields :id, :entered_at, :left_at, :area, :encrypted_data
+        field :id
+        field :area, :string do
+          formatted_value do
+            bindings[:view].link_to(
+              value&.name,
+              bindings[:view].show_path(model_name: 'area', id: value&.id)
+            )
+          end
+          export_value do
+            value&.name
+          end
+        end
+        field :entered_at
+        field :left_at
+        field :encrypted_data
+
+        export do
+          configure :id do
+            hide
+          end
+        end
       end
     end
   end
