af2ad12c656046ee095f8f8a0d02edfb533c3cd2
salimhb <salim@railslove.com>
2020-05-20 20:17:50 +0200

    adds a public request of Area and returns info including company_if (#18)
    needed to display privacy notice when checking in at a new company.

--- app/models/area.rb
+++ app/models/area.rb
@@ -2,10 +2,11 @@ class Area < ApplicationRecord
   include ApiSerializable
   include RailsAdminConfig::ForArea
 
-  EXPOSED_ATTRIBUTES = %i[id name menu_link]
+  EXPOSED_ATTRIBUTES = %i[id name menu_link company_id]
 
   belongs_to :company
   has_many :tickets
 
   delegate :menu_link, to: :company
+  delegate :id, to: :company, prefix: :company
 end
