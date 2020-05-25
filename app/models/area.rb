2639e1ac2c4699b5c8155c4e49d7b6a0b17de224
Bonflintstone <freddy@railslove.com>
2020-05-25 12:22:03 +0200

    Adds company name to area

--- app/models/area.rb
+++ app/models/area.rb
@@ -2,11 +2,12 @@ class Area < ApplicationRecord
   include ApiSerializable
   include RailsAdminConfig::ForArea
 
-  EXPOSED_ATTRIBUTES = %i[id name menu_link company_id]
+  EXPOSED_ATTRIBUTES = %i[id name menu_link company_id company_name]
 
   belongs_to :company
   has_many :tickets
 
   delegate :menu_link, to: :company
   delegate :id, to: :company, prefix: :company
+  delegate :name, to: :company, prefix: :company
 end
