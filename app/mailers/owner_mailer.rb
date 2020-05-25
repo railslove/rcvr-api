a1a375acacaf003b84fe93b4aefe127e6ed6d8c7
Daniel <dasky@railslove.com>
2020-05-25 19:28:37 +0200

    update confirmation_instructions layout

--- app/mailers/owner_mailer.rb
+++ app/mailers/owner_mailer.rb
@@ -1,7 +1,5 @@
 class OwnerMailer < Devise::Mailer
-  helper :application
-  include Devise::Controllers::UrlHelpers
 
   default template_path: 'devise/mailer'
-  default from: 'info@rcvr.app'
+  default from: 'recover <info@rcvr.app>'
 end
