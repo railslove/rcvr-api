b0eacf668dc26df068eab3d9154693841036cd07
Bonflintstone <freddy@railslove.com>
2020-05-27 09:51:31 +0200

    Remove prod fallbacks and use 2.7.1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,10 +63,6 @@ Rails.application.configure do
   }
   config.action_mailer.raise_delivery_errors = true
 
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = true
-
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
