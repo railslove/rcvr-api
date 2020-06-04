9f6b88e18b6a2bb175889cb0b7353c3f8550f5e7
salimhb <salim@railslove.com>
2020-06-04 15:02:47 +0200

    Workaround for Segfault for ruby 2.7.x (#27)
    based on https://github.com/sass/sassc-ruby/issues/197#issuecomment-634558999

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,3 +7,9 @@ Rails.application.config.assets.version = '1.0'
 
 # foundation_emails for our inky mail templates
 Rails.application.config.assets.precompile += %w[foundation_emails.css]
+
+# Workaround for Segfault
+# https://github.com/sass/sassc-ruby/issues/197
+Rails.application.config.assets.configure do |env|
+  env.export_concurrent = false
+end
