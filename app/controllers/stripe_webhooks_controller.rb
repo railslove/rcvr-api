d2cb86f5ad2c884e811e55763b5c5089d1bfbae4
Bonflintstone <freddy@railslove.com>
2020-06-05 12:39:32 +0200

    Permit stripe webhook params so we can serialize them

--- app/controllers/stripe_webhooks_controller.rb
+++ app/controllers/stripe_webhooks_controller.rb
@@ -1,9 +1,15 @@
 class StripeWebhooksController < ApplicationController
   def create
     if Rails.env.development?
-      HandleStripeWebhookJob.perform_now(params, request.body.read, request.headers['HTTP_STRIPE_SIGNATURE'])
+      HandleStripeWebhookJob.perform_now(webhook_params, request.body.read, request.headers['HTTP_STRIPE_SIGNATURE'])
     else
-      HandleStripeWebhookJob.perform_later(params, request.body.read, request.headers['HTTP_STRIPE_SIGNATURE'])
+      HandleStripeWebhookJob.perform_later(webhook_params, request.body.read, request.headers['HTTP_STRIPE_SIGNATURE'])
     end
+  end
+
+  private
+
+  def webhook_params
+    params.permit!
   end
 end
