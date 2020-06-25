c02795aed1de365eb855c1152f3006f3830eb19d
Bonflintstone <freddy@railslove.com>
2020-06-25 11:09:29 +0200

    Fix for env in test

--- spec/requests/owners/checkouts_request_spec.rb
+++ spec/requests/owners/checkouts_request_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Owners::AreasController do
 
   before { allow(Stripe::Subscription).to receive(:update) }
   before { allow(Stripe::Checkout::Session).to receive(:create) }
+  before { ENV['STRIPE_SUBSCRIPTION_PRICE_ID'] = 'test_price' }
 
   before { sign_in(owner) }
 
