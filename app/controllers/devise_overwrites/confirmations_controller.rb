3f1fb314cbf3c987762207515a98fbfe8ee4ef8e
Bonflintstone <freddy@railslove.com>
2020-07-03 08:24:34 +0200

    Hotfix confirmation

--- app/controllers/devise_overwrites/confirmations_controller.rb
+++ app/controllers/devise_overwrites/confirmations_controller.rb
@@ -1,22 +1,22 @@
 module DeviseOverwrites
   class ConfirmationsController < Devise::ConfirmationsController
     def show
-      self.owner = resource_class.confirm_by_token(params[:confirmation_token])
-      yield owner if block_given?
+      self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+      yield resource if block_given?
 
-      respond_with_navigational(owner) do
-        redirect_to(after_confirmation_path_for(resource_name, owner))
+      respond_with_navigational(resource) do
+        redirect_to(after_confirmation_path_for(resource_name, resource))
       end
     end
 
     private
 
-    def after_confirmation_path_for(resource_name, owner)
-      error_query = { error: owner.errors.full_messages }.to_param
-      path = owner.public_key.blank? ?
+    def after_confirmation_path_for(resource_name, resource)
+      error_query = { error: resource.errors.full_messages }.to_param
+      path = resource.public_key.blank? ?
         "/business/setup/key-intro" : "/business/dashboard"
 
-      URI(owner.frontend_url).tap do |uri|
+      URI(resource.frontend_url).tap do |uri|
         uri.query = error_query
         uri.path = path
       end.to_s
