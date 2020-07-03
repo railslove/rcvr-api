ac96eb76e17c23e48ba4dbe74057365964c6763a
Bonflintstone <freddy@railslove.com>
2020-07-03 08:38:41 +0200

    Fix frontend redirect link after confirmation

--- app/controllers/devise_overwrites/confirmations_controller.rb
+++ app/controllers/devise_overwrites/confirmations_controller.rb
@@ -14,7 +14,7 @@ module DeviseOverwrites
     def after_confirmation_path_for(resource_name, resource)
       error_query = { error: resource.errors.full_messages }.to_param
       path = resource.public_key.blank? ?
-        "/business/setup/key-intro" : "/business/dashboard"
+        "/business/setup/success" : "/business/dashboard"
 
       URI(resource.frontend_url).tap do |uri|
         uri.query = error_query
