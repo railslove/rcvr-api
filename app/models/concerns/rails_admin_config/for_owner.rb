d71172927fc5ec1c1f11263c3d28a2b4aeceaa58
Bonflintstone <freddy@railslove.com>
2020-07-07 11:57:51 +0200

    Configure rails admin properly :)

--- app/models/concerns/rails_admin_config/for_owner.rb
+++ app/models/concerns/rails_admin_config/for_owner.rb
@@ -4,9 +4,9 @@ module RailsAdminConfig
 
     included do
       rails_admin do
-        fields :id, :email, :created_at, :name, :companies, :affiliate, :can_use_for_free, :trial_ends_at, :block_at, :frontend
+        fields :id, :email, :created_at, :name, :companies, :affiliate, :can_use_for_free, :trial_ends_at, :block_at, :frontend, :stripe_subscription_id, :stripe_customer_id
 
-        fields :stripe_subscription_id, :stripe_customer_id, :stripe_subscription_status do
+        field :stripe_subscription_status do
           read_only true
         end
         
