550d603aede3ca23740ecbec0feccc2b30e46117
Bonflintstone <freddy@railslove.com>
2020-07-13 17:28:13 +0200

    Check stripe webhook time frame only in controller

--- app/controllers/stripe_webhooks_controller.rb
+++ app/controllers/stripe_webhooks_controller.rb
@@ -1,9 +1,21 @@
 class StripeWebhooksController < ApplicationController
   def create
+    payload = request.body.read
+    sig_header = request.headers['HTTP_STRIPE_SIGNATURE']
+    endpoint_secret = ENV['STRIPE_WEBHOOK_SIGNING_SECRET']
+
+    begin
+      Stripe::Webhook.construct_event(payload, sig_header, endpoint_secret)
+    rescue JSON::ParserError => e
+      return status 400
+    rescue Stripe::SignatureVerificationError => e
+      return status 400
+    end
+
     if Rails.env.development?
-      HandleStripeWebhookJob.perform_now(webhook_params, request.body.read, request.headers['HTTP_STRIPE_SIGNATURE'])
+      HandleStripeWebhookJob.perform_now(payload, sig_header)
     else
-      HandleStripeWebhookJob.perform_later(webhook_params, request.body.read, request.headers['HTTP_STRIPE_SIGNATURE'])
+      HandleStripeWebhookJob.perform_later(payload, sig_header)
     end
   end
 
--- app/jobs/handle_stripe_webhook_job.rb
+++ app/jobs/handle_stripe_webhook_job.rb
@@ -1,6 +1,8 @@
 class HandleStripeWebhookJob < ApplicationJob
-  def perform(params, body, stripe_signature)
-    event = Stripe::Webhook.construct_event(body, stripe_signature, ENV['STRIPE_WEBHOOK_SIGNING_SECRET'])
+  def perform(body, stripe_signature)
+    # We already check the request in the controller, to make sure that jobs that previously
+    # failed or are in the queue for long are still handled correctly we set the tolerance this high
+    event = Stripe::Webhook.construct_event(body, stripe_signature, ENV['STRIPE_WEBHOOK_SIGNING_SECRET'], tolerance: 1.month.seconds.to_i)
 
     StripeWebhookHandler.call(event: event)
   end
