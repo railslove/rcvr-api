e4a0c0ddea4095ee0282d9526a170df56ede2ca8
Bonflintstone <freddy@railslove.com>
2020-07-20 13:01:57 +0200

    Adds custom wording to care emails

--- spec/factories/frontend.rb
+++ spec/factories/frontend.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :frontend do
     url { Faker::Internet.url }
     name { Faker::Company.name }
+
+    trait :care do
+      url { 'https://care.rcvr.app' }
+    end
   end
 end
