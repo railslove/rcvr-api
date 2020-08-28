0e29d2cfa0e65ecd113cbb06ec63075b194fa62d
Bonflintstone <freddy@railslove.com>
2020-08-28 18:10:23 +0200

    Adds menu pdf to rails admin

--- app/models/concerns/rails_admin_config/for_company.rb
+++ app/models/concerns/rails_admin_config/for_company.rb
@@ -3,8 +3,11 @@ module RailsAdminConfig
     extend ActiveSupport::Concern
 
     included do
+      attr_accessor :remove_menu_pdf
+      after_save { menu_pdf.purge if remove_menu_pdf.present? }
+
       rails_admin do
-        fields :id, :name, :created_at, :areas, :owner, :tickets, :menu_link, :is_free
+        fields :id, :name, :created_at, :areas, :owner, :tickets, :menu_link, :menu_pdf, :is_free
 
         field :stats_url do
           read_only true
