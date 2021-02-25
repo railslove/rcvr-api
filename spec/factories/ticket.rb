be4d6b9951177d68126f7aed9e9e193bedd331e8
Finke, Andreas <andreas.finke@gmail.com>
2021-02-25 22:29:29 +0000

    #6 Enables updating of encrypted_data field.

--- spec/factories/ticket.rb
+++ spec/factories/ticket.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     id { Faker::Internet.uuid }
     entered_at { Faker::Time.between(from: 2.hours.ago, to: 1.hour.ago) }
     left_at { Faker::Time.between(from: 1.hour.ago, to: Time.zone.now - 1.minute) }
-
+    encrypted_data {Faker::Alphanumeric.alphanumeric}
     area
 
     trait :open do
