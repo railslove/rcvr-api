3d0c3be1bf63d2778c186167e10e0713871e98ad
Finke, Andreas <andreas.finke@gmail.com>
2021-03-18 20:14:58 +0000

    Uses secure compare.

--- app/controllers/iris/iris_controller.rb
+++ app/controllers/iris/iris_controller.rb
@@ -1,6 +1,9 @@
 class Iris::IrisController < ApplicationController
 
+  include ActiveSupport::SecurityUtils
+
   @@WFD_API_KEY_HEADER = 'x-wfd-api-key'
+  @@WFD_API_KEY = ENV['X_WFD_API_KEY']
 
   before_action :authenticate
 
@@ -8,7 +11,7 @@ class Iris::IrisController < ApplicationController
 
   def authenticate
 
-    if ENV['X_WFD_API_KEY'].nil? or ! ENV['X_WFD_API_KEY'].eql?(request.headers[@@WFD_API_KEY_HEADER]) then
+    if ! secure_compare(@@WFD_API_KEY, request.headers[@@WFD_API_KEY_HEADER] || '') then
       render status: 401
     end
 
