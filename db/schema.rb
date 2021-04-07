05ffe7ca534925e123baefe7eb13d72e1c8afc6a
Andreas Finke <hey-johnnypark@users.noreply.github.com>
2021-04-07 09:27:18 +0100

    #6 Enables updating of encrypted_data field.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_153445) do
+ActiveRecord::Schema.define(version: 2021_03_18_214915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -128,6 +128,16 @@ ActiveRecord::Schema.define(version: 2021_03_15_153445) do
     t.uuid "area_id"
     t.boolean "accepted_privacy_policy"
     t.index ["area_id"], name: "index_tickets_on_area_id"
+  end
+
+  create_table "versions", force: :cascade do |t|
+    t.string "item_type", null: false
+    t.uuid "item_id", null: false
+    t.string "event", null: false
+    t.string "whodunnit"
+    t.text "object"
+    t.datetime "created_at"
+    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
