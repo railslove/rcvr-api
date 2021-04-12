77ff4de64776541ecd16038a53cf5be77a735975
Bodo Tasche <bodo@wannawork.de>
2021-04-12 11:50:33 +0200

    joined migrations

--- db/migrate/20210412064028_drop_jwt_blacklists.rb
+++
@@ -1,5 +0,0 @@
-class DropJwtBlacklists < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :jwt_blacklists
-  end
-end
--- db/migrate/20210412064405_add_expiration_time_to_jwt_denylist.rb
+++
@@ -1,5 +0,0 @@
-class AddExpirationTimeToJwtDenylist < ActiveRecord::Migration[6.0]
-  def change
-    add_column :jwt_denylist, :exp, :datetime
-  end
-end
