8a0fa7be64a6d0646494544add2e7f73469a70d4
Bodo Tasche <bitboxer@users.noreply.github.com>
2021-04-26 13:48:56 +0200

    Do not update paused stripe payments (#103)

--- spec/interactors/stripe_webhook_handler_spec.rb
+++ spec/interactors/stripe_webhook_handler_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe StripeWebhookHandler do
   before { allow(Stripe::Subscription).to receive(:update) }
-  before { allow(Stripe::Subscription).to receive(:retrieve).and_return(double(status: :active)) }
+  before { allow(Stripe::Subscription).to receive(:retrieve).and_return(double(status: :active, pause_collection: nil)) }
 
   context 'checkout.session.completed' do
     let(:owner) { FactoryBot.create(:owner, block_at: 1.day.from_now) }
