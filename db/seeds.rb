e156c5b91997b23c8dc09f7d0522550c711eac64
peter huyser <huyserp@gmail.com>
2021-05-13 20:12:47 +0200

    seed db with 20 owners and 7 affiliates

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,27 @@
+Affiliate.destroy_all
+Owner.destroy_all
 
+7.times do
+  FactoryBot.create(:affiliate)
+end
+affiliates = Affiliate.all
+affiliates.each { |a| puts "#{a.name} #{a.code}" }
+
+20.times do
+  FactoryBot.create(:owner)
+end
+owners = Owner.all
+owners.each { |o| puts o.name }
+
+
+# give 9 owners an affilate
+owners_with_affiliates = owners[0..8]
+owners_with_affiliates.each do |owner|
+  owner.update!(affiliate: affiliates.sample.code)
+end
+
+# give 3 owners affiliates which are not yet entities in the db
+owners_with_other_affiliates = owners[9..11]
+owners_with_other_affiliates.each do |owner|
+  owner.update!(affiliate: affiliates.sample.code.prepend('a').chop)
+end
