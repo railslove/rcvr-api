ccfb883944d2fbb7d834bad179e810d82987f134
Bodo Tasche <bodo@wannawork.de>
2021-05-27 12:53:16 +0200

    Add locations to IRIS index
    closes https://github.com/railslove/recover-backlog/issues/128

---
+++ app/jobs/iris_update_company.rb
@@ -0,0 +1,32 @@
+class IrisUpdateCompany < ApplicationJob
+  queue_as :default
+
+  def perform(company_id)
+    return if ENV["IRIS_EPS_URL"].blank?
+
+    company = Company.find(company_id)
+    return unless company
+
+    Jimson::Client.new(
+      ENV["IRIS_EPS_URL"], {id_type: :string}, "ls-1.", {verify_ssl: false}
+    ).postLocationsToSearchIndex({
+      locations: [
+        {
+          id: company.id, 
+          name: company.name, 
+          contact: {
+            officalName: company.name, 
+            representative: company.owner.name, 
+            address: {
+              street: company.street, 
+              city: company.city, 
+              zip: company.zip
+            }, 
+            email: company.owner.email,
+            phone: company.owner.phone
+          }
+        }
+      ]
+    })
+  end
+end
