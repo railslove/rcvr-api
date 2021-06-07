0568ce6bdce391823f1637a924b4754e1d0fd759
Bodo Tasche <bitboxer@users.noreply.github.com>
2021-06-07 18:37:20 +0200

    Added company count to the affiliate backend (#126)
    * Added company count to the affiliate backend
    
    Closes https://github.com/railslove/recover-backlog/issues/152
    
    * Update app/models/concerns/rails_admin_config/for_affiliate.rb
    
    Co-authored-by: salimhb <salim@railslove.com>
    
    * Update app/models/concerns/rails_admin_config/for_affiliate.rb
    
    Co-authored-by: salimhb <salim@railslove.com>
    
    Co-authored-by: salimhb <salim@railslove.com>

--- app/models/concerns/rails_admin_config/for_affiliate.rb
+++ app/models/concerns/rails_admin_config/for_affiliate.rb
@@ -15,6 +15,14 @@ module RailsAdminConfig
             path = bindings[:view].index_path(model_name: 'Owner')
             bindings[:view].link_to(bindings[:object].owner_count, "#{path}?f[affiliate][1][o]=is&f[affiliate][1][v]=#{ERB::Util.url_encode(bindings[:object].code)}")
           end
+          read_only true
+        end
+        field :companies_count do
+          label "Companies"
+          formatted_value do
+            bindings[:object].company_count
+          end
+          read_only true
         end
         fields :stripe_price_id_monthly, :custom_trial_phase, :logo_link
 
