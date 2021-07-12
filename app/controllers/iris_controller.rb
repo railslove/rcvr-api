192c51a390c68d87e7c14b653fd3b7147ade5d4d
Niklas Jaeger <niklas@railslove.com>
2021-07-12 14:01:23 +0200

    change locationID to locationId

--- app/controllers/iris_controller.rb
+++ app/controllers/iris_controller.rb
@@ -4,9 +4,9 @@ class IrisController
   extend Jimson::Handler
 
   def createDataRequest(params)
-    Rails.logger.debug("Connected to Create date request controller, params:#{params}")
+    Rails.logger.debug("Connected to Create date request controller, params:#{params["dataRequest"]}")
     c_id=""
-    c_id="6f7797bf-fb83-4a5b-9b7d-e659b6d65701" if(params["dataRequest"]["locationID"]=="1abc") 
+    c_id="6f7797bf-fb83-4a5b-9b7d-e659b6d65701" if(params["dataRequest"]["locationId"]=="1abc")
     company = Company.find(c_id)
     Rails.logger.debug("Found company, company:#{company}")
     CreateDataRequest.call(
