e365c02cba17ecb66bef94c13cd4de2316c990a0
Niklas Jaeger <niklas@railslove.com>
2021-07-15 15:22:58 +0200

    change company url in iris controller

--- app/controllers/iris_controller.rb
+++ app/controllers/iris_controller.rb
@@ -6,7 +6,7 @@ class IrisController
   def createDataRequest(params)
     Rails.logger.debug("Connected to Create date request controller, params:#{params["dataRequest"]}")
     c_id=""
-    c_id="6f7797bf-fb83-4a5b-9b7d-e659b6d65701" if(params["dataRequest"]["locationId"]=="1abc")
+    c_id="6e69ea48-7eb7-42fa-a876-84e409cb5505" if(params["dataRequest"]["locationId"]=="1abc")
     company = Company.find(c_id)
     Rails.logger.debug("Found company, company:#{company}")
     CreateDataRequest.call(
