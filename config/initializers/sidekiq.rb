9117e4c8e858a354bd0d2ee5c9fdbebe7e43d5d6
Niklas Jaeger <niklas@railslove.com>
2021-07-29 13:42:02 +0200

    change redis options in sidekiq config

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,7 @@
+require 'sidekiq'
+require 'sidekiq/web'
+require 'sidekiq-scheduler/web'
+
+sidekiq.configure_server do |config|
+  config.redis = { url: ENV['REDIS_URL'], verify_mode: OpenSSL::SSL::VERIFY_NONE }
+end
