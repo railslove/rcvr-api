fcc39344d6e11925e28e34c3d30e8d3d47679f6b
Niklas Jaeger <niklas@railslove.com>
2021-07-29 14:23:09 +0200

    change sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,5 +3,8 @@ require 'sidekiq/web'
 require 'sidekiq-scheduler/web'
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'], verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  config.redis = { url: ENV['REDIS_URL'], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }  }
+end
+Sidekiq.configure_client do |config|
+  config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
 end
